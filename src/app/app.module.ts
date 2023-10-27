731aab47f580b10d12d608a174b5af6508d393ea
Berke Erçetin <82932880+berkeercetin@users.noreply.github.com>
2023-10-27 14:52:38 +0300

    First İnit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp({"projectId":"burcger-90213","appId":"1:1024765534757:web:7d8f077aa3281c87ec15cc","storageBucket":"burcger-90213.appspot.com","locationId":"us-central","apiKey":"AIzaSyAt_7Yk_NqFMiz4-FExvGR5mFbVAizsfnA","authDomain":"burcger-90213.firebaseapp.com","messagingSenderId":"1024765534757","measurementId":"G-J3SB43FLRR"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
