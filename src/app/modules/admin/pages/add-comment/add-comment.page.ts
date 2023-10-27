fee7b1d3c9e02c4595e536cbc2b3b8d82679ac8b
FrdnAkyrk1 <frdnakyrk08@gmail.com>
2023-10-28 01:41:44 +0300

    BUGFIX: fixed lint problems

--- src/app/modules/admin/pages/add-comment/add-comment.page.ts
+++ src/app/modules/admin/pages/add-comment/add-comment.page.ts
@@ -1,28 +1,16 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { 
-  Firestore, 
-  query, 
-  collection, 
-  collectionData, 
-  collectionSnapshots, 
-  getDocs,
-  doc,
-  addDoc, 
-  updateDoc } from '@angular/fire/firestore'
-
-import { FormBuilder, FormControl } from '@angular/forms';
-
+import { Component, OnInit, inject } from '@angular/core'
+import { Firestore, collection, addDoc } from '@angular/fire/firestore'
 
 @Component({
   selector: 'app-add-comment',
   templateUrl: './add-comment.page.html',
-  styleUrls: ['./add-comment.page.scss'],
+  styleUrls: ['./add-comment.page.scss']
 })
 export class AddCommentPage implements OnInit {
-  public alertButtons = ['Tamam'];
-  private firestore: Firestore = inject(Firestore);
+  public alertButtons = ['Tamam']
+  private readonly firestore: Firestore = inject(Firestore)
 
-  constructor() { }
+  constructor () { }
 
   // async addComment(zodiac: string, commentType: string, title: string, photoUrl: string, comment: string){
   //   const docRef = await addDoc(doc(this.firestore, 'zodiacComment'), {
@@ -35,17 +23,16 @@ export class AddCommentPage implements OnInit {
   //   console.log("Document written with ID: ", docRef.id);
   // }
 
-  addData(addComment: any){
-
-    const collectionInstance = collection(this.firestore, 'zodiac-comment');
-    addDoc(collectionInstance, addComment.value).then(() =>{
-      console.log('Data Saved.');
+  addData (addComment: any) {
+    const collectionInstance = collection(this.firestore, 'zodiac-comment')
+    addDoc(collectionInstance, addComment.value).then(() => {
+      console.log('Data Saved.')
     })
-    .catch((err)=>{
-      console.log(err)
-    })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
-  ngOnInit() {
+  ngOnInit () {
   }
 }
--- src/app/modules/admin/pages/edit-comment/edit-comment.page.ts
+++ src/app/modules/admin/pages/edit-comment/edit-comment.page.ts
@@ -1,25 +1,24 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { 
-  Firestore, 
-  query, 
-  collection, 
+import { Component, OnInit, inject } from '@angular/core'
+import {
+  Firestore,
+  query,
+  collection,
   getDocs,
-  doc, 
-  updateDoc } from '@angular/fire/firestore';
-import { FormBuilder, FormControl } from '@angular/forms';
-import { Observable, Subscription } from 'rxjs';
+  doc,
+  updateDoc
+} from '@angular/fire/firestore'
+import { FormBuilder, FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-edit-comment',
   templateUrl: './edit-comment.page.html',
-  styleUrls: ['./edit-comment.page.scss'],
+  styleUrls: ['./edit-comment.page.scss']
 })
 export class EditCommentPage implements OnInit {
-
-  private firestore: Firestore = inject(Firestore);
-  public alertButtons = ['Uptaded'];
-  zodiacComment: any[]=[]; 
-  //zodiacSub: Subscription;
+  private readonly firestore: Firestore = inject(Firestore)
+  public alertButtons = ['Uptaded']
+  zodiacComment: any[] = []
+  // zodiacSub: Subscription;
 
   form = this.formBuilder.group({
     title: new FormControl(''),
@@ -28,19 +27,23 @@ export class EditCommentPage implements OnInit {
     comment: new FormControl(''),
     zodiac: new FormControl('')
   })
-  
-  constructor(private formBuilder: FormBuilder) {
-    const zodiacCommentCollection = collection(this.firestore, 'zodiac-comment');
-    const zodiacCommentQuery = query(zodiacCommentCollection) as any;
-  
-    getDocs(zodiacCommentQuery).then(
-      res=>{
-        console.log(res.docs.map(doc=>doc.data()))
-        this.zodiacComment = res.docs.map(doc=>doc.data())
-        //console.log(this.zodiacComment)
-        this.form.patchValue(this.zodiacComment[0])
-      }
-    )
+
+  constructor (private readonly formBuilder: FormBuilder) {
+    const zodiacCommentCollection = collection(this.firestore, 'zodiac-comment')
+    const zodiacCommentQuery = query(zodiacCommentCollection) as any
+
+    getDocs(zodiacCommentQuery)
+      .then(
+        res => {
+          console.log(res.docs.map(doc => doc.data()))
+          this.zodiacComment = res.docs.map(doc => doc.data())
+          // console.log(this.zodiacComment)
+          this.form.patchValue(this.zodiacComment[0])
+        }
+      )
+      .catch(err => {
+        console.error(err)
+      })
 
     // //subscribe({
     //   next: res=>{
@@ -53,27 +56,26 @@ export class EditCommentPage implements OnInit {
 
     //   }
     // })
-
   }
 
-  ngOnInit() {
+  ngOnInit () {
   }
 
-  updateData(){
+  updateData () {
     const docInstance = doc(this.firestore, 'zodiac-comment', 'fyPqsn656j90qyq9tLBX')
     console.log(this.form.value)
     updateDoc(docInstance,
       this.form.value
-      ).then(
-       res=>{
-       console.log(res)
-       console.log('Update Completed')
-     }).catch(
-       err=>{
-       console.log(err)
-     })
+    ).then(
+      res => {
+        console.log(res)
+        console.log('Update Completed')
+      }).catch(
+      err => {
+        console.log(err)
+      })
   }
-  
+
   // ngOnDestroy(){ //subscribe kullanılan yerlerde kullanılır.
   //   if(this.zodiacSub && !this.zodiacSub.closed){
   //     this.zodiacSub.unsubscribe();
