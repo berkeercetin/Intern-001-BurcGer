afbe5e82a75a1d2d648c6ceafc65de78ebb9c5eb
Hakan İSMAİL <hakanismail53@gmail.com>
2023-10-31 12:26:08 +0300

    Services and New pages

---
+++ src/app/modules/main/models/comment.model.ts
@@ -0,0 +1,17 @@
+export class CommentModel {
+  commentWriterUid?: string
+  commentRequesterUid?: string
+  commentStar?: string
+  commentWriteDate?: string
+  commentRequestDate?: string
+  commentTitle?: string
+  commentZodiac?: string
+  commentType?: string
+  commentText?: string
+  commentDay?: number
+  commentWeek?: number
+  commentMonth?: number
+  commentYear?: number
+  commentId?: string
+  burImg?: string[]
+}
--- src/app/modules/main/pages/comment-request/segments/written-comments/written-comments.page.ts
+++ src/app/modules/main/pages/comment-request/segments/written-comments/written-comments.page.ts
@@ -1,22 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-written-comments',
   templateUrl: './written-comments.page.html',
-  styleUrls: ['./written-comments.page.scss'],
+  styleUrls: ['./written-comments.page.scss']
 })
 export class WrittenCommentsPage implements OnInit {
+  isDropdownOpen: boolean = false
 
-
-  isDropdownOpen: boolean = false;
-
-  toggleDropdown() {
-    this.isDropdownOpen = !this.isDropdownOpen;
+  toggleDropdown () {
+    this.isDropdownOpen = !this.isDropdownOpen
   }
 
-  constructor() { }
+  constructor () {
 
-  ngOnInit() {
   }
 
+  ngOnInit () {
+  }
 }
