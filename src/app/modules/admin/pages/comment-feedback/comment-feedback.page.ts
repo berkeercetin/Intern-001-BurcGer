f1bae23abbde5834f37e4b7a7e6fe30d134e2b5b
FrdnAkyrk1 <frdnakyrk08@gmail.com>
2023-10-31 14:33:14 +0300

    DEVELOPMENT: Comment Feedback page has been completed. Project background color has been editeed
    The backgrounds of the pages have been edited and the design and services of the comment feedback page have been completed.

--- src/app/modules/admin/pages/comment-feedback/comment-feedback.page.ts
+++ src/app/modules/admin/pages/comment-feedback/comment-feedback.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, inject } from '@angular/core'
+import { Firestore, collection, collectionData, query, getDocs } from '@angular/fire/firestore'
+import { Observable } from 'rxjs'
 @Component({
   selector: 'app-comment-feedback',
   templateUrl: './comment-feedback.page.html',
-  styleUrls: ['./comment-feedback.page.scss'],
+  styleUrls: ['./comment-feedback.page.scss']
 })
 export class CommentFeedbackPage implements OnInit {
+  private readonly firestore: Firestore = inject(Firestore)
+  feedbacks: any[] = []
+  feedback$: Observable<[]>
+  constructor () {
+    const feedbacksCollection = collection(this.firestore, 'comment-feedback')
+    this.feedback$ = collectionData(feedbacksCollection) as Observable<[]>
+    const commentFeedbackQuery = query(feedbacksCollection) as any
 
-  constructor() { }
-
-  ngOnInit() {
+    getDocs(commentFeedbackQuery)
+      .then(
+        res => {
+          console.log(res.docs.map(doc => doc.data()))
+          this.feedbacks = res.docs.map(doc => doc.data())
+        }
+      )
+      .catch((err: any) => {
+        console.error(err)
+      })
   }
 
+  ngOnInit () {
+  }
 }
